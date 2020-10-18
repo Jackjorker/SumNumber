3ca042afba905284005eb7c21c74b22b9e5f7c3f
Jackjorker <iyouthreethousands@163.com>
2020-10-18 21:12:08 +0800

    C++ Primer Plus的第一个程序

---
+++ myfirst/myfirst.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    cout<<"come up and C++ me some time."<<endl;
+    cout<<endl;
+    cout<<"you won't regret for it."<<endl;
+    return 0;
+}
